26c51b7b8bb7ce8120aa671b68544bda86a807db
rivasXimenes <rivaldoaraujo971@gmail.com>
2021-04-05 13:35:02 -0300

    aula1

--- aulaListaEx1.php
+++ aulaListaEx1.php
@@ -1,5 +1,12 @@
 <?php
 
+/* 
+Supondo que A, B e C são variáveis do tipo inteiro, 
+com valores iguais a 5, 10 e -8, respectivamente e uma 
+variável D com valor 1.5, quais os resultados das expressões 
+aritméticas apresentadas em seguida?  
+*/
+
 $a = 5;
 $b = 10;
 $c = -8;
